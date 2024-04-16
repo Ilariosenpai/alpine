c4e77e23490f20d100c0a0361e4aca11ee60e778
Ilariosenpai <ilanabougit2@gmail.com>
2024-04-16 16:54:37 +0200

    first commit

---
+++ src/component/navbar/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../sources-homepage/logo/logo.png';
+import './navbar.css';
+
+
+function Navbar() {
+
+    return (
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> <img src={logo}/> </a>
+    <a href='configurateur.js'> <button type="button" class="btn btn-outline-primary">Configurateur</button> </a>
+   
+   
+  </div>
+</nav>
+
+    );
+}
+
+export default Navbar;
---
+++ src/component/video/video.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './video.css';
+
+
+const Video = () => {
+    return (
+        <div>
+            <video src="headervid.mp4"  
+            
+            autoPlay
+            loop
+            muted
+            
+            />
+        </div>
+    );
+};
+
+export default Video;
